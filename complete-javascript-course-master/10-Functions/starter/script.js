7020ab663b1795dd8163cffe105a754ec871eed7
ddomarecki <damian.domarecki@gmail.com>
2023-03-12 15:51:14 +0100

    callback function exercise - transformer function

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -56,3 +56,24 @@
 
 //130
 //131 functions acceptiog callback function
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+
+transformer('Javascript is the best!', oneWord);
