5a2050265d09a6bfb088d29f62880256ab4edc34
ddomarecki <damian.domarecki@gmail.com>
2023-03-18 20:02:07 +0100

    More Closure Examples

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -301,20 +301,61 @@
 
 // (() => console.log('This will also never run again'))();
 
-// CLOSURES
+// CLOSURES (theory in notepad)
 
-const secureBooking = function () {
-  let passengerCount = 0;
-  return function () {
-    passengerCount++;
-    console.log(`{${passengerCount} passengers}`);
-  };
+// const secureBooking = function () {
+//   let passengerCount = 0;
+//   return function () {
+//     passengerCount++;
+//     console.log(`{${passengerCount} passengers}`);
+//   };
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+// More Closure Examples
+
+// // Example 1
+// let f;
+
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+
+// h();
+// f();
+
+// Example 2
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup}`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
 };
 
-const booker = secureBooking();
-
-booker();
-booker();
-booker();
-
-console.dir(booker);
+const perGroup = 1000;
+boardPassengers(180, 3);
