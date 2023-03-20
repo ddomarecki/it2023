91bf98cb93e51e32ba6a7764d22ffddaf4874700
ddomarecki <damian.domarecki@gmail.com>
2023-03-20 22:54:39 +0100

    Looping Arrays: forEach method

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -107,13 +107,34 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // // JOIN
 // console.log(letters.join(' - '));
 
-// 143. The new at Method
+// // 143. The new at Method
 
-const arr = [23, 11, 64];
-console.log(arr.at(0));
+// const arr = [23, 11, 64];
+// console.log(arr.at(0));
 
-console.log(arr[arr.length - 1]);
-console.log(arr.slice(-1)[0]);
-console.log(arr.at(-1));
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
 
-console.log('jonas'.at(0));
+// console.log('jonas'.at(0));
+
+// 144. Looping Arrays: forEach
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1} Your desposit ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1} your withdrew ${Math.abs(movement)}`);
+  }
+}
+
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1} Your desposit ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1} your withdrew ${Math.abs(movement)}`);
+  }
+});
