857cc1ef0271752a3665a2e3f3dcbe38970d05cf
ddomarecki <damian.domarecki@gmail.com>
2023-03-20 22:17:01 +0100

    The new at Method

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -77,32 +77,43 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // Section 11: Working With Arrays
 
-// 142. Simple Array Methods
+// // 142. Simple Array Methods
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr = ['a', 'b', 'c', 'd', 'e'];
 
-// SLICE
-arr.slice(2);
-arr.slice(2, 4);
-arr.slice(-2);
-arr.slice(-1);
-arr.slice(1, -2);
-arr.slice();
+// // SLICE
+// arr.slice(2);
+// arr.slice(2, 4);
+// arr.slice(-2);
+// arr.slice(-1);
+// arr.slice(1, -2);
+// arr.slice();
 
-// SPLICE
-arr.splice(2);
-arr.splice(-1);
-arr.splice(1, 2);
-arr.splice(1, 0, 'b', 'c');
+// // SPLICE
+// arr.splice(2);
+// arr.splice(-1);
+// arr.splice(1, 2);
+// arr.splice(1, 0, 'b', 'c');
 
-// REVERSE
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-arr2.reverse();
+// // REVERSE
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// arr2.reverse();
 
-// CONCAT
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
+// // CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
 
-// JOIN
-console.log(letters.join(' - '));
+// // JOIN
+// console.log(letters.join(' - '));
+
+// 143. The new at Method
+
+const arr = [23, 11, 64];
+console.log(arr.at(0));
+
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('jonas'.at(0));
