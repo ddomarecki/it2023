1f6f062b5af42144a33ee739add80152460ec9be
ddomarecki <damian.domarecki@gmail.com>
2023-03-24 20:49:57 +0100

    Coding Challenge #3 - rewrite function from Challenge #3 as an arrow function, and using chaining

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -387,13 +387,27 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // 155. The Magic of Chainging Methods
 
-const eurToUsd = 1.1;
-const totalDepositsUSD = movements
-  .filter(mov => mov > 0)
-  // .map(mov => mov * eurToUsd)
-  .map((mov, i, arr) => {
-    // console.log(arr);
-    return mov * eurToUsd;
-  })
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(totalDepositsUSD);
+// const eurToUsd = 1.1;
+// const totalDepositsUSD = movements
+//   .filter(mov => mov > 0)
+//   // .map(mov => mov * eurToUsd)
+//   .map((mov, i, arr) => {
+//     // console.log(arr);
+//     return mov * eurToUsd;
+//   })
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(totalDepositsUSD);
+
+// 156. Coding Challenge #3
+
+const calcAverageHumanAge = ages =>
+  Math.round(
+    ages
+      .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+      .filter(age => age >= 18)
+      .reduce((acc, age, i, arr) => acc + age / arr.length, 0)
+  );
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
