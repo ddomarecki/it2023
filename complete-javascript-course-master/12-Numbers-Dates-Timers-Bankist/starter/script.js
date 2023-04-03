7a444aa3848cfcd9fd5d7b75fce7658969f8a649
ddomarecki <damian.domarecki@gmail.com>
2023-04-03 14:28:10 +0200

    bg fixed - proper EUR unit added at Lecture 179 - 523 line of code

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -520,7 +520,7 @@ const num = 3884764.23;
 const options = {
   style: 'currency',
   unit: 'mile-per-hour',
-  currency: 'EUR ',
+  currency: 'EUR',
   // useGrouping: false
 };
 console.log(new Intl.NumberFormat('en-US', options).format(num));
