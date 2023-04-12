79b38b9b4f4de460d96dabbb6b18e21e9c60a5ca
ddomarecki <damian.domarecki@gmail.com>
2023-04-12 22:05:48 +0200

    186. Selecting, Creating, and Deleting Elements

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -38,3 +38,42 @@ document.addEventListener('keydown', function (e) {
 // 184. PROJECT: "Bankist" Website
 
 // 185. How the DOM Really Works
+
+// 186. Selecting, creating, and deleting elements
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.headt);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+
+document.getElementsByClassName('btn');
+
+// Creating and inserting elements
+
+// .insertAdjacentHTML;
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = "We use cookies for improved functionality and analytics.";
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn--close--cookie">Got it!</button>';
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.after(message)
+
+// Delete elements
+document
+  .querySelector('.btn--close--cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    // message.parentElement.removeChild(message)
+    // old method above
+  });
