22d0448b603bfd0244007d574bb304ce6a603723
ddomarecki <damian.domarecki@gmail.com>
2023-04-14 17:33:01 +0200

    189. Types of Events and Event Handlers

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -159,3 +159,24 @@ btnScrollTo.addEventListener('click', function (e) {
   // new way:
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+// 189. Types of Events and Event Handlers
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+  // h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// older method:
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
+
+// 3 sposób of handling addEventListener:
+// <h1 onclick="alert('XD')">
