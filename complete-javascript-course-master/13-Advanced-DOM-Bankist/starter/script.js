956373e82f5ebb219746f62f58dbdd0af83b974e
ddomarecki <damian.domarecki@gmail.com>
2023-04-14 18:59:34 +0200

    191. Event Propagation in Practice

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -183,3 +183,35 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
 // 190. Event Propagation: Bubbling and Capturing
 // theory, an introduction to propagation presented
+
+// 191. Event Propagation in Practice
+
+// rgb(255,255,255);
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+// console.log(randomColor());
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  },
+  false
+);
