83e1a9a67f67385f7fd2ff73d4f64bc4dfe90cc0
ddomarecki <damian.domarecki@gmail.com>
2023-04-21 15:45:04 +0200

    209. Prototypes

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -12,9 +12,9 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 
   // Never do this
-  this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  };
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
 };
 
 const damian = new Person('Damian', 1986);
@@ -25,3 +25,21 @@ const jack = new Person('jack', 1975);
 console.log(damian, matilda, jack);
 
 console.log(damian instanceof Person);
+
+// 209. Prototypes
+// adding function to Person constructor:
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+damian.calcAge();
+
+console.log(damian.__proto__);
+console.log(damian.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(damian));
+
+Person.prototype.species = 'Homo Sapiens';
+
+console.log(damian.hasOwnProperty('firstName'));
+console.log(damian.hasOwnProperty('species'));
