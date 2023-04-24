b474178e395e8de3b097d3328577c653f8975068
ddomarecki <damian.domarecki@gmail.com>
2023-04-24 20:24:17 +0200

    219. Coding Challenge #3

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -153,30 +153,31 @@ account.latest = 50;
 
 // 216. Object.create
 
-const PersonProto = {
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  },
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   },
 
-  init(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  },
-};
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
 
-const steven = Object.create(PersonProto);
-steven.name = 'Steven';
-steven.birthYear = 2002;
+// const steven = Object.create(PersonProto);
+// steven.name = 'Steven';
+// steven.birthYear = 2002;
 
-console.log(steven.__proto__ === PersonProto);
+// console.log(steven.__proto__ === PersonProto);
 
-const sarah = Object.create(PersonProto);
+// const sarah = Object.create(PersonProto);
 
-sarah.init('Sarah', 1979);
-sarah.calcAge();
+// sarah.init('Sarah', 1979);
+// sarah.calcAge();
 
-Student.prototype.constructor = Student;
-console.dir(Student.prototype.constructor);
+// Student.prototype.constructor = Student;
+// console.dir(Student.prototype.constructor);
+
 // 218. Inheritance Between "Classes": Constructor Functions
 
 const Person = function (firstName, birthYear) {
@@ -219,23 +220,23 @@ console.log(mike instanceof Object);
 ///////////////////////////////////////////////
 
 // 212. Coding Challenge #1
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-  this.speed = this.speed + 10;
-  console.log(this.speed);
-};
+// Car.prototype.accelerate = function () {
+//   this.speed = this.speed + 10;
+//   console.log(this.speed);
+// };
 
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(this.speed);
-};
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(this.speed);
+// };
 
-const car1 = new Car('BMW', 120);
-const car2 = new Car('Merceder', 95);
+// const car1 = new Car('BMW', 120);
+// const car2 = new Car('Merceder', 95);
 
 // 217. Coding Challenge #2
 
@@ -264,3 +265,45 @@ class CarlCl {
 }
 
 const car1000 = new CarlCl('Ford', 120);
+
+// 219. Coding Challenge #3
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed = this.speed + 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+// const car1 = new Car('BMW', 120);
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// Linking prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(
+    `Tesla going at ${this.speed}km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const car1 = new EV('Tesla', 120, 23);
+console.log(car1);
