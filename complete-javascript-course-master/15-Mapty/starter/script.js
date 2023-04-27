cfebe92e9470712b64158bc39b0fb8ae1233db79
ddomarecki <damian.domarecki@gmail.com>
2023-04-27 13:28:06 +0200

    233. Displaying a Map Using Leaflet Library

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -15,14 +15,31 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 // 232. Using the Geolocation API
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    console.log(position);
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
-    console.log(`https://www.google.pl/maps/@${latitude},${longitude}`);
-  },
-  function () {
-    alert('Could not get your position');
-  }
-);
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.pl/maps/@${latitude},${longitude}`);
+
+      // 233. Displaying a Map Using Leaflet Library
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
