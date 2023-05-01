629622e37736c0c5b10836206ad7870533e57aed
ddomarecki <damian.domarecki@gmail.com>
2023-05-01 15:22:23 +0200

    246. Asynchronous JavaScript, AJAX and APIs - theory

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -4,3 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+// new url: https://restcountries.com/v2/
+
+// 246. Asynchronous JavaScript, AJAX and APIs
