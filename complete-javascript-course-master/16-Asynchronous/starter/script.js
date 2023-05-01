3b74646c8b8e5519f285db0c1c388944b7d8940e
ddomarecki <damian.domarecki@gmail.com>
2023-05-01 16:12:52 +0200

    248. Our First AJAX Call: XMLHttpRequest

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -6,5 +6,38 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 // new url: https://restcountries.com/v2/
+// https://restcountries.com/v2/name/portugal
 
 // 246. Asynchronous JavaScript, AJAX and APIs
+
+// 248. Our First AJAX Call: XMLHttpRequest
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+  // console.log(request);
+
+  request.addEventListener('load', function () {
+    // console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText);
+    // zamiaast this.rewsponseText mozna request.responseText
+    const html = ` <article class="country">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+</article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('spain');
+getCountryData('poland');
+getCountryData('germany');
