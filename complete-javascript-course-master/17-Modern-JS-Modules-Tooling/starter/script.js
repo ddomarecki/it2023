60e0569b1517a508e1eadca853a552163d4aee93
ddomarecki <damian.domarecki@gmail.com>
2023-05-12 14:56:53 +0200

    274. The Module Pattern (old way to create modules)

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -40,3 +40,33 @@ console.log(lastPost); // promise pending result
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+
+// module pattern
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+
+    const orderStock = function (product, quantity) {
+      console.log(`${quantity} ${product} ordered from supplier`);
+    };
+    return {
+      addToCart,
+      cart,
+      totalPrice,
+      totalQuantity,
+    };
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2;
+
+console.log(ShoppingCart2);
+console.log(ShoppiingCart2.shippingCost); // undefined becouse private
