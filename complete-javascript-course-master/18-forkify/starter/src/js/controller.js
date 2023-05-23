6862e63b6d6958c937611c467c90770b508a3c05
ddomarecki <damian.domarecki@gmail.com>
2023-05-23 16:41:57 +0200

    295. Implementing Error and Success Messages

--- complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -32,7 +32,7 @@ const controlRecipes = async function () {
     // 2. rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
 controlRecipes();
