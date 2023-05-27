6d8680ff8acb1583f5d46d122c28c08d4c3efdf2
ddomarecki <damian.domarecki@gmail.com>
2023-05-27 14:41:41 +0200

    297. Implementing Search Results - Part 2

--- complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -1,10 +1,15 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 // import icons from '../img/icons.svg'; // Parcel 1
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -37,6 +42,7 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     // 1) get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -46,6 +52,7 @@ const controlSearchResults = async function () {
 
     // 3) render results
     console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
